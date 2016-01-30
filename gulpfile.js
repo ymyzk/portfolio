58cbd7908c094b2dfcf05b78f8c4ebc341fa39ae
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-01-30 14:48:37 +0900

    Add gulpfile.js & move index.css

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var argv = require("yargs").argv;
+var autoprefixer = require("gulp-autoprefixer");
+var cssnano = require("gulp-cssnano");
+var gulp = require("gulp");
+var gulpif = require("gulp-if");
+var sass = require("gulp-sass");
+
+const PRODUCTION = !!argv.production;
+
+const OUTPUT = "output/";
+const STATIC = "static/";
+
+gulp.task("scss", () =>
+  gulp.src(`${STATIC}stylesheets/**/*.scss`)
+    .pipe(sass({
+      outputStyle: PRODUCTION ? "compressed" : "expanded",
+      sourceComments: !PRODUCTION
+    }))
+    .pipe(autoprefixer({
+      browsers: [
+        "Last 2 versions",
+        // Bootstrap
+        "Android 2.3",
+        "Android >= 4",
+        "Chrome >= 20",
+        "Firefox >= 24",
+        "Explorer >= 8",
+        "iOS >= 6",
+        "Opera >= 12",
+        "Safari >= 6"
+      ]
+    }))
+    .pipe(gulpif(PRODUCTION, cssnano()))
+    .pipe(gulp.dest(`${OUTPUT}stylesheets/`))
+);
+
+gulp.task("stylesheets", ["scss"]);
+
+gulp.task("build", ["stylesheets"]);
+
+gulp.task("default", ["build"]);
