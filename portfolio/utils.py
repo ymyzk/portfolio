0e93f94ca9d14f87cf06627a19c50ca1423d3d8f
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-02-25 06:09:36 +0900

    Add data.py

--- portfolio/utils.py
+++ portfolio/utils.py
@@ -1,13 +1,5 @@
 from datetime import date
-from operator import itemgetter
-from os import path
-from typing import Any, Dict, List, Tuple
-
-import yaml
-
-
-root_dir = path.dirname(path.dirname(path.abspath(__file__)))
-data_dir = root_dir + "/data/"
+from typing import Dict
 
 
 def calc_age() -> int:
@@ -23,46 +15,3 @@ def calc_copyright_years() -> Dict[str, int]:
         "start": 2013,
         "end": date.today().year
     }
-
-
-def load_skills() -> List[str]:
-    with open(data_dir + "skills.yml") as f:
-        skills = yaml.load(f)
-    return skills
-
-
-def load_projects() -> List[Dict[str, Any]]:
-    def format(work: Dict[str, Any]) -> Dict[str, Any]:
-        if work["end"] is None:
-            # 継続中のプロジェクトなら "YYYY-"
-            work["duration"] = work["start"].strftime("%Y-")
-        else:
-            # 終了済みのプロジェクトなら "YYYY" または "YYYY-YYYY"
-            work["duration"] = work["start"].strftime("%Y")
-            if work["start"].year < work["end"].year:
-                work["duration"] += "-" + work["end"].strftime("%Y")
-        return work
-
-    with open(data_dir + "projects.yml") as f:
-        works = yaml.load(f)
-
-    return [format(work) for work in works]
-
-
-def load_talks() -> List[Dict[str, Any]]:
-    with open(data_dir + "talks.yml") as f:
-        talks = yaml.load(f)
-    return talks
-
-
-def load_contributions() -> List[Dict[str, Any]]:
-    with open(data_dir + "contributions.yml") as f:
-        contributions = yaml.load(f)
-    contributions.sort(key=itemgetter("name"))
-    return contributions
-
-
-def load_links() -> Tuple[List[Dict[str, str]], List[Dict[str, str]]]:
-    with open(data_dir + "links.yml") as f:
-        links, links2 = yaml.load_all(f)
-    return links, links2
--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from functools import partial
 import sys
 
+from portfolio import data
 from portfolio.views import Context, html_view as _html_view
-from portfolio.utils import (calc_age, load_contributions, load_links,
-                             load_projects, load_skills, load_talks)
+from portfolio.utils import calc_age
 
 
 production = "--production" in sys.argv
@@ -16,36 +16,36 @@ html_view = partial(_html_view, debug=debug)
 def index() -> Context:
     return {
         "age": calc_age(),
-        "skills": load_skills(),
-        "links": load_links()[0]
+        "skills": data.load_skills(),
+        "links": data.load_links()[0]
     }
 
 
 @html_view("projects.html")
 def projects() -> Context:
     return {
-        "projects": load_projects()
+        "projects": data.load_projects()
     }
 
 
 @html_view("talks.html")
 def talks() -> Context:
     return {
-        "talks": load_talks()
+        "talks": data.load_talks()
     }
 
 
 @html_view("contributions.html")
 def contributions() -> Context:
     return {
-        "contributions": load_contributions()
+        "contributions": data.load_contributions()
     }
 
 
 @html_view("news.html")
 def news() -> Context:
     return {
-        "news": load_links()[1]
+        "news": data.load_links()[1]
     }
 
 
