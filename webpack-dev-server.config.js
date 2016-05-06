701e4a752366384278812beb35476529d632e4ef
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-05-07 04:32:04 +0900

    Fix styles

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -1,37 +1,37 @@
-const webpack = require('webpack');
-const path = require('path');
-const buildPath = path.resolve(__dirname, 'build');
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
-const TransferWebpackPlugin = require('transfer-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const buildPath = path.resolve(__dirname, "build");
+const nodeModulesPath = path.resolve(__dirname, "node_modules");
+const TransferWebpackPlugin = require("transfer-webpack-plugin");
 
 const config = {
   entry: [
-    'webpack/hot/dev-server',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, '/src/javascripts/entry.js')
+    "webpack/hot/dev-server",
+    "webpack/hot/only-dev-server",
+    path.join(__dirname, "/src/javascripts/entry.js")
   ],
   resolve: {
     extensions: ["", ".js", ".jsx", ".scss", ".yml"]
   },
   devServer: {
-    contentBase: 'output',
-    devtool: 'eval',
+    contentBase: "output",
+    devtool: "eval",
     hot: true,
     inline: true,
     port: 3000,
     // router 用
     historyApiFallback: true
   },
-  devtool: 'eval',
+  devtool: "eval",
   output: {
     path: buildPath,
-    filename: 'bundle.js'
+    filename: "bundle.js"
   },
   module: {
     preLoaders: [
       {
         test: /\.(js|jsx)$/,
-        loader: 'eslint-loader',
+        loader: "eslint-loader",
         include: [path.resolve(__dirname, "src/javascripts")],
         exclude: [nodeModulesPath]
       }
@@ -43,12 +43,12 @@ const config = {
       },
       {
         test: /\.(js|jsx)$/,
-        loaders: ['react-hot', 'babel'],
+        loaders: ["react-hot", "babel"],
         exclude: [nodeModulesPath]
       },
       {
         test: /\.yml$/,
-        loaders: ['json', 'yaml']
+        loaders: ["json", "yaml"]
       }
     ]
   },
@@ -56,7 +56,7 @@ const config = {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new TransferWebpackPlugin([
-      { from: 'src/www' }
+      { from: "src/www" }
     ])
   ],
   sassLoader: {
