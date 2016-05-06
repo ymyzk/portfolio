989579d3b61451ebc571590d0ff8967d01827b2b
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-05-07 04:31:07 +0900

    Rename files: .jsx -> .js

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -8,7 +8,7 @@ const config = {
   entry: [
     'webpack/hot/dev-server',
     'webpack/hot/only-dev-server',
-    path.join(__dirname, '/src/javascripts/entry.jsx')
+    path.join(__dirname, '/src/javascripts/entry.js')
   ],
   resolve: {
     extensions: ["", ".js", ".jsx", ".scss", ".yml"]
