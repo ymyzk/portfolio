c4474b8418c5138d270b07ee8b823d8fa3db1bdf
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-05-08 06:53:59 +0900

    Add routes.js

--- src/javascripts/entry.js
+++ src/javascripts/entry.js
@@ -1,16 +1,9 @@
 import React from "react";
 import injectTapEventPlugin from "react-tap-event-plugin";
-
 import { render } from "react-dom";
-import { Router, Route, browserHistory, IndexRoute } from "react-router";
+import { Router, browserHistory } from "react-router";
 
-import App from "./components/App";
-import Home from "./components/Home";
-import Contributions from "./components/Contributions";
-import News from "./components/News";
-import Projects from "./components/Projects";
-import Talks from "./components/Talks";
-import PageNotFound from "./components/PageNotFound";
+import routes from "./routes";
 
 // Needed for onTouchTap
 // Can go away when react 1.0 release
@@ -35,14 +28,5 @@ browserHistory.listen(location => {
 });
 
 render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-      <Route path="projects" component={Projects} />
-      <Route path="talks" component={Talks} />
-      <Route path="contributions" component={Contributions} />
-      <Route path="news" component={News} />
-      <Route path="*" component={PageNotFound} />
-    </Route>
-  </Router>
+  <Router history={browserHistory} routes={routes} />
 ), document.getElementById("app"));
---
+++ src/javascripts/routes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, IndexRoute } from "react-router";
+
+import App from "./components/App";
+import Home from "./components/Home";
+import Contributions from "./components/Contributions";
+import News from "./components/News";
+import Projects from "./components/Projects";
+import Talks from "./components/Talks";
+import PageNotFound from "./components/PageNotFound";
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home} />
+    <Route path="projects" component={Projects} />
+    <Route path="talks" component={Talks} />
+    <Route path="contributions" component={Contributions} />
+    <Route path="news" component={News} />
+    <Route path="*" component={PageNotFound} />
+  </Route>
+);
