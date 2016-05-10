b85b28dc0c397564e18ad35a565209fbf3b58f74
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-05-10 09:27:03 +0900

    Generate sitemap

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const autoprefixer = require("autoprefixer");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const StaticSiteGeneratorPlugin = require("static-site-generator-webpack-plugin");
 const TransferWebpackPlugin = require("transfer-webpack-plugin");
+const SitemapGeneratorPlugin = require("./plugins/sitemap");
 
 const DEBUG = !process.argv.includes("--production");
 const CLIENT = !process.argv.includes("--server");
@@ -11,6 +12,7 @@ const CLIENT = !process.argv.includes("--server");
 const buildPath = path.resolve(__dirname, "build/" + (CLIENT ? "client" : "server"));
 const nodeModulesPath = path.resolve(__dirname, "node_modules");
 
+const base_path = "https://www.ymyzk.com";
 const paths = [
   "/",
   "/projects/",
@@ -40,7 +42,7 @@ const config = {
   devtool: DEBUG ? "eval" : "source-map",
   output: {
     path: buildPath,
-    publicPath: DEBUG ? "/" : "https://www.ymyzk.com/",
+    publicPath: DEBUG ? "/" : base_path,
     filename: "bundle.js",
     libraryTarget: CLIENT ? "var" : "commonjs2"
   },
@@ -105,6 +107,7 @@ const config = {
     ] : [
       new StaticSiteGeneratorPlugin("bundle.js", paths)
     ]),
+    new SitemapGeneratorPlugin(base_path, paths, "sitemap.xml"),
     new ExtractTextPlugin("bundle.css")
   ],
   postcss: function () {
