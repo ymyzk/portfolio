4c059c6eba556fcf95f9411b7824006898583de1
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-06-15 11:33:47 +0900

    Update contributions

--- src/javascripts/components/Contributions.js
+++ src/javascripts/components/Contributions.js
@@ -11,6 +11,10 @@ const Contributions = () => {
     <div className="container">
       <Helmet title={title} />
       <h2>{title}</h2>
+      <p>
+        I'm interested in the open source community and software.
+        The following are some of the open source communities and software which I contributed to.
+      </p>
       <div className="grid">
         <div className="cell-xs-without-gutter cell-sm-12">
           <List>
