a7f24d75db2ba678900eea1aaf903744f87a5253
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-04-10 03:16:37 +0900

    Refactor Links

--- src/javascripts/components/Home/Links.js
+++ src/javascripts/components/Home/Links.js
@@ -9,24 +9,35 @@ const floatingButtonStyle = {
   margin: "0 6px 6px",
 };
 
+const Link = ({ link }) => {
+  let icon;
+  if (link.icon) {
+    icon = <FontIcon className={`fa fa-${link.icon}`} />;
+  } else {
+    icon = <FontIcon className="fa fa-globe" />;
+  }
+  return (
+    <FloatingActionButton
+      href={link.url}
+      target="_blank"
+      style={floatingButtonStyle}
+      backgroundColor={link.color}
+    >
+      { icon }
+    </FloatingActionButton>
+  );
+};
+
+Link.propTypes = {
+  link: CustomPropTypes.Link.isRequired,
+};
+
 const Links = ({ links }) => (
   <section className="grid">
     <div className="cell-sm-12">
       <h2>Links</h2>
       <div className="text-center">
-        {
-          links.map(l => (
-            <FloatingActionButton
-              key={l.url}
-              href={l.url}
-              target="_blank"
-              style={floatingButtonStyle}
-              backgroundColor={l.color}
-            >
-              <FontIcon className={`fa fa-${l.icon ? l.icon : "globe"}`} />
-            </FloatingActionButton>
-          ))
-        }
+        { links.map(l => <Link key={l.url} link={l} />) }
       </div>
     </div>
   </section>
