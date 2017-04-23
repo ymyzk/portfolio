6d4b3dfc3d1003b20250f173aed72d9b38aef1c8
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-04-23 16:17:24 +0900

    Update stats.js for CDN

--- plugins/stats.js
+++ plugins/stats.js
@@ -8,12 +8,13 @@ StatsPlugin.prototype.apply = function(compiler) {
   const self = this;
 
   compiler.plugin("emit", function(compilation, callback) {
+    const publicPath = compiler.options.output.publicPath;
     const hash = compilation.hash;
     const result = {
       hash,
       files: {
-        bundleCss: `bundle.${hash}.css`,
-        bundleJs: `bundle.${hash}.js`
+        bundleCss: `${publicPath}bundle.${hash}.css`,
+        bundleJs: `${publicPath}bundle.${hash}.js`
       }
     };
     const out = JSON.stringify(result);
