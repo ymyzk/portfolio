65654dc20d470c4bf0f23710b62201151cde18f4
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-04-28 17:28:22 +0900

    Add Affiliation.js

--- src/javascripts/components/Home.js
+++ src/javascripts/components/Home.js
@@ -1,47 +1,11 @@
-import FontIcon from "material-ui/FontIcon";
-import List from "material-ui/List/List";
-import ListItem from "material-ui/List/ListItem";
 import React from "react";
 
 import Activities from "../containers/Home/Activities";
+import Affiliation from "../components/Home/Affiliation";
 import Links from "../containers/Home/Links";
 import Skills from "../containers/Home/Skills";
 import Top from "./Home/Top";
 
-const iconStyle = {
-  top: 0,
-  bottom: 0,
-  margin: "auto 12px",
-  textAlign: "center",
-};
-
-const Affiliation = () => (
-  <section className="grid">
-    <div className="cell-xs-without-gutter cell-sm-12">
-      <h2>Affiliation</h2>
-      <List>
-        <ListItem
-          leftIcon={<FontIcon className="fa fa-university" style={iconStyle} />}
-          primaryText={"University"}
-          secondaryText={"Graduate School of Informatics, Kyoto University"}
-          href={"http://www.fos.kuis.kyoto-u.ac.jp/~miyazaki/"}
-        />
-        <ListItem
-          leftIcon={<FontIcon className="fa fa-building-o" style={iconStyle} />}
-          primaryText={"Company"}
-          secondaryText={"President & Co-founder, Unimap, Inc."}
-          href={"http://www.unimap.co.jp/"}
-        />
-        <ListItem
-          leftIcon={<FontIcon className="fa fa-group" style={iconStyle} />}
-          primaryText={"Community"}
-          secondaryText={"CAMPHOR-"}
-          href={"https://camph.net/"}
-        />
-      </List>
-    </div>
-  </section>
-);
 
 const Home = () => (
   <div className="home">
