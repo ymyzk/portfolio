89fde0b86694a570a645ba6ffdc426f6b80f04a6
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-07-20 02:18:42 +0900

    Remove zopfli option used in CompressionPlugin

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -165,8 +165,6 @@ const config = (env) => {
         new webpack.optimize.UglifyJsPlugin(),
         new webpack.optimize.AggressiveMergingPlugin(),
         new CompressionPlugin({
-          // NOTE: requires node-zopfli, you can specify gzip if you don't have it
-          // algorithm: "zopfli",
           test: /\.(css|eot|js|svg|ttf|txt|xml)$/,
           threshold: 1024,
           minRatio: 0.8,
