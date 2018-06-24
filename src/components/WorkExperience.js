dc52b613882b7e223d84a9839746c00da147e182
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-06-24 19:55:18 +0900

    Implement expandable work experience list

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -3,7 +3,10 @@ import React from "react";
 
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const WorkExperienceItem = ({ work }) => (
   <ListItem button component="a" href={work.link}>
@@ -18,14 +21,43 @@ WorkExperienceItem.propTypes = {
   work: PropTypes.object.isRequired,
 };
 
-const WorkExperience = ({ works }) => (
-  <List>
-    { works.map(w => <WorkExperienceItem work={w} key={w.title + w.company} />) }
-  </List>
-);
+class WorkExperience extends React.Component {
+  static propTypes = {
+    works: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  };
 
-WorkExperience.propTypes = {
-  works: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
-};
+  state = {
+    expanded: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      expanded: !this.state.expanded,
+    });
+  };
+
+  render() {
+    const { works } = this.props;
+    const { expanded } = this.state;
+    return (
+      <List>
+        {
+          works
+            .filter(w => (expanded ? true : w.featured))
+            .map(w => <WorkExperienceItem work={w} key={w.title + w.company} />)
+        }
+        <ListItem button>
+          <ListItemIcon>
+            {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          </ListItemIcon>
+          <ListItemText
+            primary={`Show ${expanded ? "less" : "more"}`}
+            onClick={this.toggle}
+          />
+        </ListItem>
+      </List>
+    );
+  }
+}
 
 export default WorkExperience;
