17b0e3bd681c42b71cd18836685481b5b177554b
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-07-02 03:21:03 +0900

    Add more meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -8,11 +8,15 @@ class MyDocument extends Document {
   render() {
     const { pageContext } = this.props;
 
+    const title = "Yusuke Miyazaki";
+    const description = "Yusuke Miyazaki's portfolio website.";
+    const url = "https://www.ymyzk.com";
+
     return (
-      <html lang="en" dir="ltr">
+      <html lang="en" dir="ltr" prefix="og: http://ogp.me/ns# profile: http://ogp.me/ns/profile# fb: http://ogp.me/ns/fb#">
         <Head>
           <title>
-            Yusuke Miyazaki
+            { title }
           </title>
           <meta charSet="utf-8" />
           {/* Use minimum-scale=1 to enable GPU rasterization */}
@@ -36,8 +40,32 @@ class MyDocument extends Document {
             href="/static/images/favicon.png"
             sizes="256x256"
           />
+          {/* SEO */}
+          <meta name="description" content={description} />
+          {/* Facebook / OGP */}
+          <meta property="fb:app_id" content="997147760366147" />
+          <meta property="og:type" content="profile" />
+          <meta property="og:title" content={title} />
+          <meta property="og:url" content={url} />
+          <meta property="og:image" content="/static/images/ogp.png" />
+          <meta property="og:locale" content="ja_JP" />
+          <meta property="og:locale:alternate" content="en_US" />
+          <meta property="og:site_name" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="profile:first_name" content="Yusuke" />
+          <meta property="profile:last_name" content="Miyazaki" />
+          <meta property="profile:username" content="ymyzk" />
+          {/* Twitter Cards */}
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@ymyzk" />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:description" content={description} />
+          <meta name="twitter:image" content="/static/images/twitter.png" />
           {/* PWA primary color */}
           <meta name="theme-color" content={pageContext.theme.palette.primary.main} />
+          {/* Windows / IE / Edge */}
+          <meta name="msapplication-TileImage" content="/static/images/ms-tile-image.png" />
+          <meta name="msapplication-TileColor" content={pageContext.theme.palette.primary.main} />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
