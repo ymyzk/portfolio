d5ee8da84ce3a5c9047b9490d2d11340d79a34ea
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-07-29 02:23:21 +0900

    Fix negative margin problem

--- src/components/ProjectCardList.js
+++ src/components/ProjectCardList.js
@@ -11,15 +11,19 @@ type Props = {
 };
 
 const ProjectCardList = ({ projects }: Props) => (
-  <Grid container spacing={16}>
-    {
-      projects.filter(p => p.featured).map(p => (
-        <Grid item key={p.id} xs={12} sm={6} md={4}>
-          <ProjectCard project={p} />
-        </Grid>
-      ))
-    }
-  </Grid>
+  // Hack to fix the problem related to negative margin
+  // https://material-ui.com/layout/grid/#negative-margin
+  <div style={{ padding: 8 }}>
+    <Grid container spacing={16}>
+      {
+        projects.filter(p => p.featured).map(p => (
+          <Grid item key={p.id} xs={12} sm={6} md={4}>
+            <ProjectCard project={p} />
+          </Grid>
+        ))
+      }
+    </Grid>
+  </div>
 );
 
 ProjectCardList.propTypes = {
