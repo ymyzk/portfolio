d90424e5e3d9777971caac4bc8f9e81c9cde5aee
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2019-03-18 02:46:45 +0900

    Partially revert 4bce031 to avoid a bug

--- pages/_document.js
+++ pages/_document.js
@@ -65,6 +65,10 @@ class MyDocument extends Document {
             name="msapplication-TileColor"
             content={pageContext ? pageContext.theme.palette.primary.main : null}
           />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
         </Head>
         <style jsx global>
           {`
@@ -74,6 +78,7 @@ class MyDocument extends Document {
 
           /* Taken from https://fonts.googleapis.com/css?family=Roboto:300,400,500 */
           /* and extended with font-display */
+          /*
           @font-face {
             font-family: 'Roboto';
             font-style: normal;
@@ -98,6 +103,7 @@ class MyDocument extends Document {
             src: local('Roboto Medium'), local('Roboto-Medium'), url(https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4AMP6lQ.woff2) format('woff2');
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
           }
+          */
           `}
         </style>
         <body>
