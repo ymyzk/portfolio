f19d0aadbd08821e799ead09bc1c587f8192f41d
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2021-05-06 16:03:54 +0900

    Refactor and fix minify-images

--- bin/minify-images.mjs
+++ bin/minify-images.mjs
@@ -6,6 +6,8 @@ import imageminMozjpeg from "imagemin-mozjpeg";
 import imageminWebp from "imagemin-webp";
 import Jimp from "jimp";
 
+const DESTINATION = "public/static/images";
+
 const resize = ({ width, height, mime }) => async (buffer, ...args) => {
   const image = await Jimp.read(buffer);
   image.resize(width, height);
@@ -31,13 +33,12 @@ const forceExtension = ext => (files) => {
     delete parsedPath.base;
     parsedPath.ext = ext;
     const newFilePath = path.format(parsedPath);
-    fs.renameSync(filePath, path.format(parsedPath));
+    fs.renameSync(filePath, newFilePath);
     return Object.assign(file, { destinationPath: newFilePath });
   });
 };
 
-async function convert(input, output, options) {
-  options.destination = output; // Compatibility for imagemin 7+
+async function convert(input, options) {
   const files = await imagemin(input, options);
   for (const postPlugin of options.postPlugins || []) {
     postPlugin(files);
@@ -48,84 +49,84 @@ async function convert(input, output, options) {
   return files;
 }
 
-(async () => {
-  await convert(["resources/images/background.jpg"], "public/static/images", {
-    plugins: [
-      resize({
-        width: 1024,
-        height: Jimp.AUTO,
-        mime: Jimp.MIME_JPEG,
-      }),
-      imageminMozjpeg({
-        quality: 70,
-      }),
-    ],
-    postPlugins: [
-      forceExtension(".jpg"),
-    ],
-  });
-  await convert(["resources/images/projects/*.png"], "public/static/images/projects", {
-    plugins: [
-      resize({
-        width: 384,
-        height: Jimp.AUTO,
-        mime: Jimp.MIME_PNG,
-      }),
-      imageminMozjpeg({
-        quality: 80,
-      }),
-    ],
-    postPlugins: [
-      forceExtension(".jpg"),
-    ],
-  });
-  await convert(["resources/images/projects/*.png"], "public/static/images/projects", {
-    plugins: [
-      resize({
-        width: 768,
-        height: Jimp.AUTO,
-        mime: Jimp.MIME_PNG,
-      }),
-      imageminMozjpeg({
-        quality: 80,
-      }),
-    ],
-    postPlugins: [
-      extendFilename("@2x"),
-      forceExtension(".jpg"),
-    ],
-  });
-  await convert(["resources/images/projects/*.png"], "public/static/images/projects", {
-    plugins: [
-      resize({
-        width: 384,
-        height: Jimp.AUTO,
-        mime: Jimp.MIME_PNG,
-      }),
-      imageminWebp({
-        quality: 75,
-        method: 6, // slowest
-      }),
-    ],
-    postPlugins: [
-      forceExtension(".webp"),
-    ],
-  });
-  await convert(["resources/images/projects/*.png"], "public/static/images/projects", {
-    plugins: [
-      resize({
-        width: 768,
-        height: Jimp.AUTO,
-        mime: Jimp.MIME_PNG,
-      }),
-      imageminWebp({
-        quality: 75,
-        method: 6, // slowest
-      }),
-    ],
-    postPlugins: [
-      extendFilename("@2x"),
-      forceExtension(".webp"),
-    ],
-  });
-})();
+await convert(["resources/images/background.jpg"], {
+  destination: DESTINATION,
+  plugins: [
+    resize({
+      width: 1024,
+      height: Jimp.AUTO,
+      mime: Jimp.MIME_JPEG,
+    }),
+    imageminMozjpeg({
+      quality: 70,
+    }),
+  ],
+  postPlugins: [
+    forceExtension(".jpg"),
+  ],
+});
+await convert(["resources/images/projects/*.png"], {
+  destination: `${DESTINATION}/projects`,
+  plugins: [
+    resize({
+      width: 384,
+      height: Jimp.AUTO,
+      mime: Jimp.MIME_JPEG,
+    }),
+    imageminMozjpeg({
+      quality: 80,
+    }),
+  ],
+  postPlugins: [
+    forceExtension(".jpg"),
+  ],
+});
+await convert(["resources/images/projects/*.png"], {
+  destination: `${DESTINATION}/projects`,
+  plugins: [
+    resize({
+      width: 768,
+      height: Jimp.AUTO,
+      mime: Jimp.MIME_JPEG,
+    }),
+    imageminMozjpeg({
+      quality: 80,
+    }),
+  ],
+  postPlugins: [
+    extendFilename("@2x"),
+    forceExtension(".jpg"),
+  ],
+});
+// For renaming files, we should write @2x first
+await convert(["resources/images/projects/*.png"], {
+  destination: `${DESTINATION}/projects`,
+  plugins: [
+    resize({
+      width: 768,
+      height: Jimp.AUTO,
+      mime: Jimp.MIME_PNG,
+    }),
+    imageminWebp({
+      quality: 75,
+      method: 6, // slowest
+    }),
+  ],
+  postPlugins: [
+    extendFilename("@2x"),
+  ],
+});
+await convert(["resources/images/projects/*.png"], {
+  destination: `${DESTINATION}/projects`,
+  plugins: [
+    resize({
+      width: 384,
+      height: Jimp.AUTO,
+      mime: Jimp.MIME_PNG,
+    }),
+    imageminWebp({
+      quality: 75,
+      method: 6, // slowest
+    }),
+  ],
+});
