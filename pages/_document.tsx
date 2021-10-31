283e51f1502abb3443dc4c2480273ef1ccd320c0
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2021-10-31 15:49:35 +0900

    Finish migration to emotion

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,9 @@ import Document, {
   Head, Html, Main, NextScript,
 } from "next/document";
 
-import { ServerStyleSheets } from "@mui/styles";
+import createEmotionServer from "@emotion/server/create-instance";
 
+import createEmotionCache from "../src/createEmotionCache";
 import theme from "../src/theme";
 import { getAssetPrefix } from "../src/utils";
 
@@ -85,6 +86,8 @@ export default class MyDocument extends Document {
   }
 }
 
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
   // Resolution order
   //
@@ -108,29 +111,45 @@ MyDocument.getInitialProps = async (ctx) => {
   // 3. app.render
   // 4. page.render
 
-  // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
 
+  // You can consider sharing the same emotion cache between all the SSR requests to speed up
+  // performance. However, be aware that it can have global side effects.
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
   ctx.renderPage = () => originalRenderPage({
-    enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    // eslint-disable-next-line react/display-name
+    enhanceApp: (App: any) => (props) => <App emotionCache={cache} {...props} />,
   });
 
   const initialProps = await Document.getInitialProps(ctx);
+  // This is important. It prevents emotion to render invalid HTML.
+  // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  const globalStyleElement = (
+    <style
+    // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: globalStyle }}
+    />
+  );
 
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: (
-      <React.Fragment key="styles">
-        { /* TODO We may have a better solution with material-ui v4 */ }
-        <style
-        // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: globalStyle }}
-        />
-        {initialProps.styles}
-        {sheets.getStyleElement()}
-      </React.Fragment>
-    ),
+    styles: [
+      globalStyleElement,
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
   };
 };
