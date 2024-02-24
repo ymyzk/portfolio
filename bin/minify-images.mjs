6df928d129abdad051e2713f3ca29016a9211bc6
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2024-02-24 22:48:26 +0900

    Replace imagemin with sharp

--- bin/minify-images.mjs
+++ bin/minify-images.mjs
@@ -1,132 +1,48 @@
-import fs from "fs";
-import path from "path";
+import { basename, extname, join, relative } from "node:path";
 
-import imagemin from "imagemin";
-import imageminMozjpeg from "imagemin-mozjpeg";
-import imageminWebp from "imagemin-webp";
-import Jimp from "jimp";
+import { glob } from "glob";
+import sharp from "sharp";
 
-const DESTINATION = "public/static/images";
+const SOURCE = "resources/images";
+const TARGET = "public/static/images";
 
-const resize = ({ width, height, mime }) => async (buffer, ...args) => {
-  const image = await Jimp.read(buffer);
-  image.resize(width, height);
-  return await image.getBufferAsync(mime);
-};
+await sharp(join(SOURCE, "background.jpg"))
+  .resize(1024, null)
+  .jpeg({
+    mozjpeg: true,
+    quality: 70,
+  })
+  .toFile(join(TARGET, "background.jpg"));
 
-const extendFilename = name => (files) => {
-  files.map((file) => {
-    const { destinationPath: filePath } = file;
-    const parsedPath = path.parse(filePath);
-    delete parsedPath.base;
-    parsedPath.name += name;
-    const newFilePath = path.format(parsedPath);
-    fs.renameSync(filePath, newFilePath);
-    return Object.assign(file, { destinationPath: newFilePath });
-  });
-};
-
-const forceExtension = ext => (files) => {
-  files.map((file) => {
-    const { destinationPath: filePath } = file;
-    const parsedPath = path.parse(filePath);
-    delete parsedPath.base;
-    parsedPath.ext = ext;
-    const newFilePath = path.format(parsedPath);
-    fs.renameSync(filePath, newFilePath);
-    return Object.assign(file, { destinationPath: newFilePath });
-  });
-};
-
-async function convert(input, options) {
-  const files = await imagemin(input, options);
-  for (const postPlugin of options.postPlugins || []) {
-    postPlugin(files);
-  }
-  for (const { destinationPath, sourcePath } of files) {
-    console.log(`Converted: ${sourcePath} => ${destinationPath}`);
-  }
-  return files;
+for (const sourceFile of await glob(join(SOURCE, "projects/*.png"))) {
+  const targetFile = join(TARGET, relative(SOURCE, sourceFile));
+  const changeExtension = (file, extension) => `${file.slice(0, -extname(file).length)}${extension}`;
+  await sharp(sourceFile)
+    .resize(384, null)
+    .jpeg({
+      mozjpeg: true,
+      quality: 80,
+    })
+    .toFile(changeExtension(targetFile, ".jpg"));
+  await sharp(sourceFile)
+    .resize(768, null)
+    .jpeg({
+      mozjpeg: true,
+      quality: 80,
+    })
+    .toFile(changeExtension(targetFile, "@2x.jpg"));
+  await sharp(sourceFile)
+    .resize(384, null)
+    .webp({
+      effort: 4,
+      quality: 75,
+    })
+    .toFile(changeExtension(targetFile, ".webp"));
+  await sharp(sourceFile)
+    .resize(768, null)
+    .webp({
+      effort: 4,
+      quality: 75,
+    })
+    .toFile(changeExtension(targetFile, "@2x.webp"));
 }
-
-await convert(["resources/images/background.jpg"], {
-  destination: DESTINATION,
-  plugins: [
-    resize({
-      width: 1024,
-      height: Jimp.AUTO,
-      mime: Jimp.MIME_JPEG,
-    }),
-    imageminMozjpeg({
-      quality: 70,
-    }),
-  ],
-  postPlugins: [
-    forceExtension(".jpg"),
-  ],
-});
-await convert(["resources/images/projects/*.png"], {
-  destination: `${DESTINATION}/projects`,
-  plugins: [
-    resize({
-      width: 384,
-      height: Jimp.AUTO,
-      mime: Jimp.MIME_JPEG,
-    }),
-    imageminMozjpeg({
-      quality: 80,
-    }),
-  ],
-  postPlugins: [
-    forceExtension(".jpg"),
-  ],
-});
-await convert(["resources/images/projects/*.png"], {
-  destination: `${DESTINATION}/projects`,
-  plugins: [
-    resize({
-      width: 768,
-      height: Jimp.AUTO,
-      mime: Jimp.MIME_JPEG,
-    }),
-    imageminMozjpeg({
-      quality: 80,
-    }),
-  ],
-  postPlugins: [
-    extendFilename("@2x"),
-    forceExtension(".jpg"),
-  ],
-});
-// For renaming files, we should write @2x first
-await convert(["resources/images/projects/*.png"], {
-  destination: `${DESTINATION}/projects`,
-  plugins: [
-    resize({
-      width: 768,
-      height: Jimp.AUTO,
-      mime: Jimp.MIME_PNG,
-    }),
-    imageminWebp({
-      quality: 75,
-      method: 6, // slowest
-    }),
-  ],
-  postPlugins: [
-    extendFilename("@2x"),
-  ],
-});
-await convert(["resources/images/projects/*.png"], {
-  destination: `${DESTINATION}/projects`,
-  plugins: [
-    resize({
-      width: 384,
-      height: Jimp.AUTO,
-      mime: Jimp.MIME_PNG,
-    }),
-    imageminWebp({
-      quality: 75,
-      method: 6, // slowest
-    }),
-  ],
-});
